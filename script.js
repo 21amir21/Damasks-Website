3f29790cf349b498928be4ee0920f21766359aa4
Amir <amirosama21@gmail.com>
2023-03-04 23:32:06 +0200

    modification to the js file

--- script.js
+++ script.js
@@ -10,10 +10,9 @@ function openNav() {
   }
   //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
   var dropdown = document.getElementsByClassName("dropdown-btn");
-  var i;
 
-  for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function () {
+  for (var i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", () => {
       this.classList.toggle("active");
       var dropdownContent = this.nextElementSibling;
       if (dropdownContent.style.display === "block") {
@@ -28,7 +27,7 @@ function openNav() {
   // this is a topUp function in Customer Service Page // Might remove it later
   const myButton = document.getElementById("myBtn");
 
-          window.onscroll = function () {
+          window.onscroll = () => {
             scrollFunction();
           };
 
